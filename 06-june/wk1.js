f1833f009cb906018d3cbd8520e99e48b4610558
nickonyi <nickoluoch1130@gmail.com>
2026-06-01 18:29:46 +0300

    some june code

--- 06-june/wk1.js
+++ 06-june/wk1.js
@@ -2,4 +2,4 @@ const arrowFunc = function (arr) {
   return arr.map((n) => String.fromCharCode(n)).join(""); //Complete this function
 };
 
-console.log(arrowFunc([23, 34, 56, 78, 98]));
+console.log(arrowFunc([98, 34, 56, 78, 98]));
