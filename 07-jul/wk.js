bec0403215e9a4e84e7d5adf509c8305b6d3a9d7
nickonyi <nickoluoch1130@gmail.com>
2026-07-23 18:42:47 +0300

    Refactor linked list functions and add push implementation

--- 07-jul/wk.js
+++ 07-jul/wk.js
@@ -6,3 +6,34 @@ function testEven(n) {
   }
   return false;
 }
+
+function Node(data) {
+  this.data = data;
+  this.next = null;
+}
+
+function push(head, data) {
+  // Go.
+  return new Node(data, head);
+}
+
+function buildOneTwoThree() {
+  // Go.
+}
+
+function length(head) {
+  // Your code goes here.
+  console.log(head);
+}
+
+function count(head, data) {
+  // Your code goes here.
+}
+
+let chained = null;
+chained = push(chained, 3);
+chained = push(chained, 2);
+chained = push(chained, 1);
+chained = push(chained, 8);
+
+console.log(chained);
